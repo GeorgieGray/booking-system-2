9f08d6443075553e2abc563311111b0af2497108
Georgie Gray <georginamirgray@gmail.com>
2023-06-24 16:26:47 +0100

    add auto login post signup

--- user/views.py
+++ user/views.py
@@ -5,3 +5,10 @@ class Signup(FormView):
     template_name = "signup.html"
     form_class = SignupForm
     success_url = "/booking"
+
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+        
+        return super(Signup, self).form_valid(form)
