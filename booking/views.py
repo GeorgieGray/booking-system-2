4330b74254dde10aab6ad9835dc3c28c3473cfd1
Georgie Gray <georginamirgray@gmail.com>
2023-06-25 21:53:18 +0100

    add booking creation form

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from .models import Booking
+from django.contrib.admin.widgets import AdminDateWidget
 
 class Home(TemplateView):
     template_name = "home.html"
 
-class Booking(TemplateView):
+class BookingDashboard(CreateView):
+    model = Booking
     template_name = "booking.html"
+    success_url = '/booking'
+    fields = ['date', 'time', 'group_size', 'table']
