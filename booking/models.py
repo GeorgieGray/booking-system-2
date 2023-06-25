a5419e6cd83907d83e3713f35fd851353a3c4a97
Georgie Gray <georginamirgray@gmail.com>
2023-06-25 21:16:42 +0100

    add table model

--- booking/models.py
+++ booking/models.py
@@ -5,3 +5,13 @@ class Restaurant(models.Model):
     table_time_limit = models.PositiveIntegerField()
     phone_number = models.CharField(max_length=32)
     name = models.CharField(max_length=32)
+
+class Table(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    max_seats = models.PositiveIntegerField()
+    min_seats = models.PositiveIntegerField()
+    table_number = models.PositiveIntegerField()
+    restaurant = models.ForeignKey(
+        "Restaurant",
+        on_delete=models.CASCADE
+    )
