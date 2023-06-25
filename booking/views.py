944ea5f8874dd4417980c1acc133e2e4f356ab01
Georgie Gray <georginamirgray@gmail.com>
2023-06-25 21:53:53 +0100

    make booking creation form pretty

--- booking/views.py
+++ booking/views.py
@@ -10,3 +10,12 @@ class BookingDashboard(CreateView):
     template_name = "booking.html"
     success_url = '/booking'
     fields = ['date', 'time', 'group_size', 'table']
+
+    def get_form(self, form_class=None):
+        form = super(BookingDashboard, self).get_form(form_class)
+        form.fields['date'].widget = AdminDateWidget(attrs={'type': 'date', 'class': 'form-control mb-3' })
+        form.fields['time'].widget.attrs['class'] = 'form-control'
+        form.fields['group_size'].widget.attrs['class'] = 'form-control'
+        form.fields['table'].widget.attrs['class'] = 'form-control'
+
+        return form
