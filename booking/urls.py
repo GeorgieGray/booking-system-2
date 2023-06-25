b3685f3cac19243d332411b2dc60fa17bd9d5737
Georgie Gray <georginamirgray@gmail.com>
2023-06-25 20:44:15 +0100

    add booking route

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, Booking
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', Home.as_view(), name="home")
+    path('', Home.as_view(), name="home"),
+    path('booking/', Booking.as_view(), name="booking")
 ]
