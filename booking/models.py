0572c393f95d65eab36d5d4115a7f80e6550f245
Georgie Gray <georginamirgray@gmail.com>
2023-06-25 21:18:05 +0100

    add booking model

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Restaurant(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -15,3 +16,48 @@ class Table(models.Model):
         "Restaurant",
         on_delete=models.CASCADE
     )
+
+class Booking(models.Model):
+    BOOKING_TIMES = [
+        ("01", "1am"),
+        ("02", "2am"),
+        ("03", "3am"),
+        ("04", "4am"),
+        ("05", "5am"),
+        ("06", "6am"),
+        ("07", "7am"),
+        ("08", "8am"),
+        ("09", "9am"),
+        ("10", "10am"),
+        ("11", "11am"),
+        ("12", "12pm"),
+        ("13", "1pm"),
+        ("14", "2pm"),
+        ("15", "3pm"),
+        ("16", "4pm"),
+        ("17", "5pm"),
+        ("18", "6pm"),
+        ("19", "7pm"),
+        ("20", "8pm"),
+        ("21", "9pm"),
+        ("22", "10pm"),
+        ("23", "11pm"),
+        ("24", "12am")
+    ]
+
+    id = models.BigAutoField(primary_key=True)
+    group_size = models.IntegerField()
+    time = models.CharField(max_length=2, choices=BOOKING_TIMES)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    table = models.ForeignKey(
+        "Table",
+        on_delete=models.CASCADE
+    )
+    note = models.TextField(blank=True)
+   
+    class Meta:
+        ordering = ["date", "time"]
