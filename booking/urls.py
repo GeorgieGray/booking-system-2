3faa7c55d2d652be029334d6726a9cf0a5032c5b
Georgie Gray <georginamirgray@gmail.com>
2023-06-25 21:58:59 +0100

    fix booking delete route typos

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import Home, BookingDashboard
+from .views import Home, BookingDashboard, DeleteBooking
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
-    path('booking/', BookingDashboard.as_view(), name="booking")
+    path('booking/', BookingDashboard.as_view(), name="booking"),
     path('booking/<pk>/delete', DeleteBooking.as_view(), name="delete_booking")
 ]
